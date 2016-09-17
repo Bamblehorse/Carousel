c24eba6c6ff8d4a2d07d7d85ff2f401ea08d7b65
Jonathan Wood <bamblehorse@gmail.com>
2016-09-17 17:53:30 +0100

    Added Jquery, added css animation for navigation, created initial JS file

---
+++ src/js/carousel.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  // Carousel component and settings
+  var $right_nav = $('.o-carousel__nav--right');
+  var $left_nav = $('.o-carousel__nav--left')
+
+  $left_nav.click(function(event) {
+
+  });
+
+  $right_nav.click(function(event) {
+
+  });
+});
+
+
+jQuery(function($) {
+
+  // settings
+  var $slides = $('.o-carousel__slides'); // ul of slides
+  var $slide = $('.o-carousel__img--current'); // each slide with image within
+});
